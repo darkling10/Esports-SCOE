371871ab7bfb8b3f25f07040b3bc8c959763a973
darkling10 <pathan.abbas10@gmail.com>
2022-02-04 11:50:26 +0530

    Footer Icons Added

---
+++ app.js
@@ -0,0 +1,9 @@
+
+function instaRedirect() {
+    window.location.href = "https://instagram.com/esports.scoe?utm_medium=copy_link";
+}
+
+
+function whatsappRedirect() {
+    window.location.href = "https://chat.whatsapp.com/JuzWjfNgfCT5EEf2BNYaOO";
+}
